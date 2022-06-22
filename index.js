63acd06ba55a5bd7b5886aa41daeea2bf9a9e0ef
Ronablee <jaaxgmd@gmail.com>
2022-06-22 00:20:14 -0300

    Added a new item route!

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) =>{
     res.send('Hello World')
 });
 
+app.get('/items', (req, res) =>{
+    res.send('Welcome to items list!')
+})
+
+
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
 });
