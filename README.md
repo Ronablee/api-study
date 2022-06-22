1863a0596d1a583cc7651b3f5124fa6eebb44736
Ronablee <jaaxgmd@gmail.com>
2022-06-22 01:54:26 -0300

    Added README and some code optimizations

---
+++ README.md
@@ -0,0 +1,3 @@
+#  A simple API in development using NodeJS.
+
+## This repository is just to see my evolution during my studies sessions
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3333;
+
+
+const routes = require('../routes')
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+app.use(routes)
